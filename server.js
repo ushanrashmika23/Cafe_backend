45ace535b45146da676a784f20befbcb865a836f
Ushan Rashmika <ushanrashmika23@gmail.com>
2024-03-15 13:35:44 +0530

    database connection successed using mongoose

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
 
-const PORT = process.env.PORT || 8877
+const PORT = process.env.PORT || 8877;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
+//dbConnection
+
+mongoose.connect(
+  "mongodb+srv://ushanrashmikacp:iI6Gambh4j46qApk@cluster0.kgaxehn.mongodb.net/ST_MAN_DB?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection successfull...");
+});
 
 //listen server
-app.listen(PORT,()=>{
-    console.log(`Server running on port :${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port :${PORT}`);
+});
