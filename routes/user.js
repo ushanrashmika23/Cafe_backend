ca0370907c520668c702c237c5a34f2e14c1563c
Ushan Rashmika <ushanrashmika23@gmail.com>
2024-03-17 18:21:27 +0530

    usrer reg done

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,49 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const user = require("./../models/user.model");
 
-const schema = mongoose.Schema;
+const router = express.Router();
 
-const userSchema = new schema({
-  username: {
-    type: String,
-    required: true,
+function Now() {
+  const today = new Date();
+  const date = today.toLocaleDateString();
+  const time = today.toLocaleTimeString("en-US", { hour12: false });
+  return { date, time };
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + Date.now() + path.extname(file.originalname));
   },
 });
 
-const user=mongoose.model("user",userSchema)
-module.exports=user;
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedFileTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+let upload = multer({ storage, fileFilter });
+
+//Reg User
+router.route("/reg").post(upload.single('photo'), (req, res) => {
+  const { username, email, password, propic } = req.body;
+  const time = Now().time;
+  const date = Now().date;
+  const userData = { username, email, password, propic, date, time };
+  const newUser= new user(userData);
+
+  newUser.save().then(()=>{res.json("User Added")}).catch((err)=>{res.json(err)})
+
+//   res.send(user);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = process.env.PORT || 8877;
+const PORT = process.env.PORT || 7080;
 
 app.use(cors());
 app.use(bodyParser.json());
